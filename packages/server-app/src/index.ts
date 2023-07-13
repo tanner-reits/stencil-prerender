cefbb5580eef92f04038b2d46a150484b083834f
Tanner Reits <tanner@ionic.io>
2023-07-13 10:46:38 -0400

    little simplification

--- packages/server-app/src/index.ts
+++ packages/server-app/src/index.ts
@@ -1,13 +1,13 @@
 import { renderToString } from "stencil-library/hydrate";
 import * as http from "http";
 
-http.createServer(onRequest).listen(8888);
-console.log("Server has started");
+const PORT = 8888;
+
+http.createServer(onRequest).listen(PORT);
+console.log(`Server has started on port ${PORT}`);
 
 async function onRequest(_request: any, response: any) {
-  const res = await renderToString(
-    '<my-component></my-component><ion-icon name="logo-twitter"></ion-icon>'
-  );
+  const res = await renderToString("<my-component></my-component>");
   response.writeHead(200);
   response.write(res.html);
   response.end();
